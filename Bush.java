6c0ec5c7a7170695211f21ea6bc4861703ed9b74
MarissaAlmoslino <45407137+MarissaAlmoslino@users.noreply.github.com>
2018-12-04 17:10:28 -0500

    Add files via upload

---
+++ Bush.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Bush extends  StationaryObject{
+
+    int numberOfBerries;
+    boolean hasBerries;
+
+    public Bush(){
+        super(true);
+        numberOfBerries = 1;
+        hasBerries = true;
+
+    }
+
+    public Bush( Vector2 position){
+        super(position, true);
+        numberOfBerries = 1;
+        hasBerries = true;
+    }
+
+    public int getNumberOfBerries(){
+        return numberOfBerries;
+    }
+
+    public boolean getHasBerries(){
+        return hasBerries;
+    }
+
+    public int gatherBerries(){
+        hasBerries = false;
+        int returnVal = numberOfBerries;
+        numberOfBerries = 0;
+        return returnVal;
+    }
+}
