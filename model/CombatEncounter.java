aac31a0f09ae1f679b7d45ca2a726932c0c48ff8
Jose J. Vargas <32646029+doublejvargas@users.noreply.github.com>
2018-12-14 00:03:28 -0500

    Add files via upload

---
+++ model/CombatEncounter.java
@@ -0,0 +1,105 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class CombatEncounter {
+
+    private Player player;
+    private Animal animal;
+    private Game game;
+    private boolean encounterOver;
+    private boolean playersTurn;
+
+
+    private boolean animalsTurn;
+
+    CombatEncounter(Player player, Animal animal){
+        this.player = player;
+        this.animal = animal;
+        playersTurn = false;
+        animalsTurn = false;
+        animal.setFled(false);
+        player.setFled(false);
+        encounterOver = false;
+        determineCombatOrder();
+        System.out.println("Combat encounter created.");
+    }
+
+    private void determineCombatOrder(){
+        Random rand = new Random();
+        int encounterOrder = rand.nextInt(2);
+        switch(encounterOrder){
+            case 0:
+                playersTurn = true;
+                break;
+            default:
+                animalsTurn = true;
+        }
+    }
+
+    public void combatTurn(){
+        if (!player.isAlive() || !animal.isAlive() || player.isFled() || animal.isFled()){
+            encounterOver = true;
+            animal.setRemove(true);
+        }
+        if(!encounterOver) {
+            if (animalsTurn && animal.isAlive()) {
+                animal.combatLogic();
+                animalsTurn = false;
+                playersTurn = true;
+            }
+        } else {
+            if (!animal.isAlive()){
+                System.out.println("Player slayed animal");
+            } else if (player.isFled()){
+                player.setPosition(TerrainTile.getBaseLocation().getDisplacementVector(5,5));
+                System.out.println("Player has fled");
+            }
+            Game.setInCombat(false);
+            Game.setCurrentEncounter(null);
+
+        }
+    }
+
+
+    public void setGame (Game game){ this.game = game;}
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public boolean isPlayersTurn() {
+        return playersTurn;
+    }
+
+    public void setPlayersTurn(boolean playersTurn) {
+        this.playersTurn = playersTurn;
+    }
+
+    public boolean isAnimalsTurn() {
+        return animalsTurn;
+    }
+
+    public void setAnimalsTurn(boolean animalsTurn) {
+        this.animalsTurn = animalsTurn;
+    }
+
+    public boolean isEncounterOver() {
+        return encounterOver;
+    }
+
+
+
+}
---
+++ model/MobileObject.java
@@ -0,0 +1,108 @@
+package model;
+
+public abstract class MobileObject extends GameObject {
+    private final int SPEED;
+    private final int MAX_HIT_POINTS;
+    private int currentHitPoints;
+    private boolean alive;
+    private boolean busy;
+    private boolean fled;
+    private Vector2 priorLoc;
+    private Game.Direction facing;
+
+
+    private boolean turnOver;
+
+    public MobileObject(Vector2 pos, int speed, int maxHitPoints){
+        super(pos);
+        priorLoc = new Vector2(getPosition().getv0(), getPosition().getv1());
+        this.SPEED = speed;
+        this.MAX_HIT_POINTS = maxHitPoints;
+        this.currentHitPoints = maxHitPoints;
+        alive = true;
+        busy = false;
+        turnOver = false;
+    }
+
+    public void sufferHarm(int harm){
+        this.setCurrentHitPoints(this.getCurrentHitPoints() - harm);
+        if (this.getCurrentHitPoints() <= 0){
+            this.setAlive(false);
+        }
+    }
+    public void move(Game.Direction direction){
+        switch(direction){
+            case RIGHT:
+                if (this.getPosition().getv1() < Game.getWIDTH()- 1) {
+                    this.setPosition(new Vector2(this.getPosition().getv0(), this.getPosition().getv1() + 1));
+                }
+                break;
+            case UP:
+                if (this.getPosition().getv0() > 0) {
+                    this.setPosition(new Vector2(this.getPosition().getv0() - 1, this.getPosition().getv1()));
+                }
+                break;
+            case DOWN:
+                if (this.getPosition().getv0() < Game.getHEIGHT() - 1) {
+                    this.setPosition(new Vector2(this.getPosition().getv0() + 1, this.getPosition().getv1()));
+                }
+                break;
+            case LEFT:
+                if (this.getPosition().getv1() > 0) {
+                    this.setPosition(new Vector2(this.getPosition().getv0(), this.getPosition().getv1() - 1));
+                }
+                break;
+        }
+    }
+
+    //GETTERS AND SETTERS
+
+    public int getMAX_HIT_POINTS() {
+        return MAX_HIT_POINTS;
+    }
+    public int getCurrentHitPoints() {
+        return currentHitPoints;
+    }
+    public void setCurrentHitPoints(int currentHitPoints) {
+        this.currentHitPoints = currentHitPoints;
+    }
+    public boolean isAlive() {
+        return alive;
+    }
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+    public int getSpeed() {
+        return SPEED;
+    }
+    public boolean isBusy() {
+        return busy;
+    }
+    public void setBusy(boolean busy) {
+        this.busy = busy;
+    }
+    public Game.Direction getFacing() {
+        return facing;
+    }
+    public void setFacing(Game.Direction facing) {
+        this.facing = facing;
+    }
+    public boolean isFled() {
+        return fled;
+    }
+    public void setFled(boolean fled){
+        this.fled = fled;
+    }
+    public Vector2 getPriorLoc() {
+        return priorLoc;
+    }
+    public void setPriorLoc(Vector2 priorLoc) {
+        this.priorLoc = priorLoc;
+    }
+    public boolean isTurnOver() {
+        return turnOver;
+    }
+    public void setTurnOver(boolean turnOver) {
+        this.turnOver = turnOver;
+    }
+}
